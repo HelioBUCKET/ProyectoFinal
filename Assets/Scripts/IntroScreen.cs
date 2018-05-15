393e0989e3e1d134d163ffd4d6e8b9fb6a8da7cf
silver1525 <davidverdugof@hotmail.com>
2018-05-15 16:00:07 +0200

    assets

---
+++ Assets/Scripts/IntroScreen.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class IntroScreen : MonoBehaviour {
+
+    public GameObject StartText;
+
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+    public Image black;
+    public Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("FlashLabel", 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.anyKey)
+        {
+
+
+            MusicSource.Play();
+            StartCoroutine("waitTime");
+        }
+    }
+
+    void FlashLabel()
+    {
+        if (StartText.activeSelf)
+            StartText.SetActive(false);
+        else
+            StartText.SetActive(true);
+    }
+
+    IEnumerator waitTime()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(()=>black.color.a==1);
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+
+}
---
+++ Assets/Scripts/SinglePlayerBtn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class SinglePlayerBtn : MonoBehaviour {
+        
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("The cursor entered the selectable UI element.");
+    }
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log("The cursor entered the selectable UI element. LOL");
+
+
+    }
+
+}
